a181d071bd1364c0c4f34f831bf50596a67688c1
Duduzera1997 <eduardojdn1997@gmail.com>
2019-04-05 16:00:52 -0300

    Create application with React.

---
+++ Section 02/react-webpack/src/app.js
@@ -0,0 +1,9 @@
+'use strict'
+var React = require('react')
+
+var Title = React.createClass({
+    render: function() {
+        return React.createElement('h1', null, 'Title React!')
+    }
+})
+module.exports = Title;
--- Section 02/react-webpack/src/index.js
+++ Section 02/react-webpack/src/index.js
@@ -1,1 +1,10 @@
-console.log('WebPack está funcionando! \\o/')
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
