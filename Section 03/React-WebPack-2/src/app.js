0457d8386bda3bf3b1a044953e3a87213fa287ba
Duduzera1997 <eduardojdn1997@gmail.com>
2019-04-05 17:49:50 -0300

    Using the examples of Props.

---
+++ Section 03/React-WebPack-2/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from './title'
+
+const App = React.createClass({
+    render: () => {
+        return (
+            <div>
+                <Title name='Eduardo J. Nascimento' />
+            </div>
+        )
+    }
+})
+export default App;
