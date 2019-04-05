6c80874803d6087556e96020a68f57797b999211
Duduzera1997 <eduardojdn1997@gmail.com>
2019-04-05 17:28:01 -0300

    Configure lint.

--- Section 02/react-webpack/webpack.config.js
+++ Section 02/react-webpack/webpack.config.js
@@ -1,9 +1,9 @@
-'use strict'
 
 const path = require('path')
 const webpack = require('webpack')
+const validate = require('webpack-validator')
 
-module.exports = {
+module.exports = validate({
     devtool: 'source-map',
     entry: [
         'react-hot-loader/patch',
@@ -22,6 +22,12 @@ module.exports = {
     ],
 
     module : {
+        preLoaders: [{
+            test: /\.js%/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader:'standard'
+        }],
         loaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
@@ -29,4 +35,4 @@ module.exports = {
             loader: 'babel'
         }]
     }
-}
+})
